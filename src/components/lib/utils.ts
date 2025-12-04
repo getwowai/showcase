734e88a63e80237d24ec6d6e9045a92c0d2ed508
Muhammad <Muhammad.Ahsan@staples.com>
2025-12-04 15:00:34 +0500

    Add basic google analytics integration

--- src/components/lib/utils.ts
+++ src/components/lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID || "";
+
+declare global {
+  interface Window {
+    gtag: (...args: unknown[]) => void;
+  }
+}
+
+export const pageview = (url: string) => {
+  if (typeof window !== "undefined" && window.gtag && GA_ID) {
+    window.gtag("config", GA_ID, {
+      page_path: url,
+      page_location: window.location.href,
+    });
+  }
+};
